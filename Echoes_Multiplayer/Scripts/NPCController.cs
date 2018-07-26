e723aa58cb7b8724bbd3b3a2586613c642fe2efe
Alexander Kessaris <ank352@nyu.edu>
2018-07-26 19:16:11 -0400

    Unfinished Multiplayer

---
+++ Echoes_Multiplayer/Scripts/NPCController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace Echoes_Multiplayer
+{
+    public class NPCController : NetworkBehaviour
+    {
+        //Targetted player
+        public Transform target;
+
+        //Players
+        public List<Transform> players;
+        public Transform player1;
+
+        //Speed of NPC
+        public float speed = 1;
+
+        private NetworkStartPosition[] spawnPoints;
+
+
+        // Use this for initialization
+        void Start()
+        {
+            players = new List<Transform>();
+            players.Add(player1);
+            target = players[0];
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            float step = speed * Time.deltaTime;
+
+            //Move NPC closer to nearest target
+            transform.position = Vector3.MoveTowards(transform.position, findTarget(target).position, step);
+            Debug.Log("Target = " + target);
+        }
+
+        private Transform findTarget(Transform target)
+        {
+            //need to find way to access all players
+            foreach (Transform player in players)
+            {
+                if (Vector3.Distance(player.position, transform.position) < Vector3.Distance(target.position, transform.position))
+                {
+                    target = player;
+                }
+            }
+            return target;
+        }
+
+        void RpcRespawn()
+        {
+            if (isLocalPlayer)
+            {
+                // Set the spawn point to origin as a default value
+                Vector3 spawnPoint = Vector3.zero;
+
+                // If there is a spawn point array and the array is not empty, pick one at random
+                if (spawnPoints != null && spawnPoints.Length > 0)
+                {
+                    spawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length)].transform.position;
+                }
+
+                // Set the player’s position to the chosen spawn point
+                GameObject.Find("Player").transform.position = spawnPoint;
+                GameObject.Find("Camera_Holder").transform.position = spawnPoint;
+            }
+        }
+    }
+}
---
+++ Echoes_Multiplayer/Scripts/PlayerController.cs
@@ -0,0 +1,75 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+using UnityEngine.EventSystems;
+
+namespace Echoes_Multiplayer
+{
+    public class PlayerController : NetworkBehaviour, MonoBehaviour
+    {
+        private GameObject cam_Holder; //holds camera since GVR overrides camera's position
+        private Camera[] cams;
+        private Camera cam;
+        private int cam_counter = 0;
+        private GameObject NPC;
+
+        public static Echoes_Multiplayer.PlayerController Instance;
+
+        private void Awake()
+        {
+            //Singleton
+            if (isLocalPlayer)
+            {
+                Destroy(this);
+            }
+            else
+            {
+                Instance = this;
+            }
+            cams = Camera.allCameras;
+            cam_Holder = GameObject.Find("Cam_Holder"); //get parent object of camera
+            GameObject.Find("Enemy").GetComponent<PlayerScript>().players.Add();
+        }
+
+        void Update()
+        {
+            if (!isLocalPlayer)
+            {
+                return;
+            }
+            //Switch cameras
+            foreach (Camera i in cams) {
+                i.enabled = false;
+            }
+            cam.enabled = true;
+
+            //Track rotation
+            transform.rotation = cam.transform.rotation;
+
+            //If trigger, fire and move
+            if (Input.GetMouseButton(0))
+            {
+                //Calculate where to move
+                Vector3 forward = transform.forward;
+                forward.y = 0;
+                Vector3 newPosition = forward * Time.deltaTime * 5.0f + transform.position;
+
+                //Constrain movement
+                newPosition.x = Mathf.Clamp(newPosition.x, -10, 10);
+                newPosition.z = Mathf.Clamp(newPosition.z, -10, 10);
+
+                //Update position
+                transform.position = newPosition; //move player
+            }
+            //Update camera (parent) position
+            cam_Holder.transform.position = transform.position;
+        }
+
+        public override void OnStartLocalPlayer()
+        {
+            GetComponent<Renderer>().material.SetColor("_Color", Color.blue); //set color of local player to blue
+            cam = cams[cam_counter++]; //set new camera
+            cam_Holder.transform.position = transform.position; //set position of camera's parent object to player
+        }
+    }
+}
